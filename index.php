06304496a2ff9596213f6fd0468353e6557d9079
FabriceJoseph <FabriceJoseph@users.noreply.github.com>
2017-06-05 14:52:09 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+front controller (chef d'orchestre) de l'application
+placer ici tous les fichiers de config (constantes, parametrage, etc.)
+
+*/
+
+require_once('config/classload.php');
+
+echo "<H1>site en construction</H1>";
+
+
+
+?>
